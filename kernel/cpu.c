328aca9b289c334b05df7ff6fd664e3a1b88d4d6
El-Nath <gamakireta@gmail.com>
2014-04-17 19:44:25 +0700

    revert "sched: Fix endless sync_sched/rcu() loop inside _cpu_down()"
    Commit:
    
    	sched: Fix endless sync_sched/rcu() loop inside _cpu_down()
    
    Since it'll causes some instability, such as lost of signal and can't
    shutdown properly.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -232,8 +232,6 @@ static int __ref take_cpu_down(void *_param)
 		return err;
 
 	cpu_notify(CPU_DYING | param->mod, param->hcpu);
-	/* Park the stopper thread */
-	kthread_park(current);
 	return 0;
 }
 
@@ -289,8 +287,8 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen)
 	err = __stop_machine(take_cpu_down, &tcd_param, cpumask_of(cpu));
 	if (err) {
 		/* CPU didn't die: tell everyone.  Can't complain. */
-		smpboot_unpark_threads(cpu);
 		cpu_notify_nofail(CPU_DOWN_FAILED | mod, hcpu);
+
 		goto out_release;
 	}
 	BUG_ON(cpu_online(cpu));
@@ -359,10 +357,6 @@ static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
 		goto out;
 	}
 
-	ret = smpboot_create_threads(cpu);
-	if (ret)
-		goto out;
-
 	ret = __cpu_notify(CPU_UP_PREPARE | mod, hcpu, -1, &nr_calls);
 	if (ret) {
 		nr_calls--;
@@ -380,9 +374,6 @@ static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
 	if (ret != 0)
 		goto out_notify;
 	BUG_ON(!cpu_online(cpu));
-
-	/* Wake the per cpu threads */
-	smpboot_unpark_threads(cpu);
 
 	/* Now call notifier in preparation. */
 	cpu_notify(CPU_ONLINE | mod, hcpu);
