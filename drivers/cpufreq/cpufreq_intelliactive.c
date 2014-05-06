f095c29b6f7d9a5f802593ff833055bcbc491b15
El-Nath <gamakireta@gmail.com>
2014-05-06 13:06:09 +0700

    cpufreq : intelliactive : lowering the high freq
    1ghz as high freq is more than enough, it's on the middlle between 81
    mhz and 1,5ghz
    
    Signed-off-by: El-Nath <gamakireta@gmail.com>

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -135,7 +135,7 @@ static unsigned int up_threshold_any_cpu_load = 97;
 static unsigned int sync_freq = 702000;
 static unsigned int up_threshold_any_cpu_freq = 918000;
 
-static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1242000} ;
+static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1026000} ;
 
 static int cpufreq_governor_intelliactive(struct cpufreq_policy *policy,
 		unsigned int event);
