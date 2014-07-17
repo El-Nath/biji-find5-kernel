2f8314736e621050da7bc175f43a0b7ef5f1cfd8
El-Nath <gamakireta@gmail.com>
2014-07-17 17:54:40 +0700

    cpuquiet: code fix-up
    due to adjustment made on kernel/sched/core.c

--- drivers/cpuquiet/governors/rq_stats.c
+++ drivers/cpuquiet/governors/rq_stats.c
@@ -66,6 +66,8 @@ static bool log_hotplugging = false;
 	if (log_hotplugging) pr_info("[RQ_STATS]: " msg); \
 	} while (0)
 	
+extern unsigned long avg_cpu_nr_running(unsigned int cpu);
+	
 static unsigned int get_lightest_loaded_cpu_n(void)
 {
 	unsigned long min_avg_runnables = ULONG_MAX;
@@ -73,7 +75,7 @@ static unsigned int get_lightest_loaded_cpu_n(void)
 	int i;
 
 	for_each_online_cpu(i) {
-		unsigned int nr_runnables = get_avg_nr_running(i);
+		unsigned int nr_runnables = avg_cpu_nr_running(i);
 
 		if (i > 0 && min_avg_runnables > nr_runnables) {
 			cpu = i;
